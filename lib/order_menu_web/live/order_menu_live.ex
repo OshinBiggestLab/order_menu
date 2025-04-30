ee49c5df2a420f85fcc18e555f74a0782ae93649
OSHIN <oshin@biggestlab.io>
2025-04-30 12:51:29 +0700

    Create & Test "is_clicked" state

--- lib/order_menu_web/live/order_menu_live.ex
+++ lib/order_menu_web/live/order_menu_live.ex
@@ -1,28 +1,20 @@
 defmodule OrderMenuWeb.OrderMenuLive do
   use OrderMenuWeb, :live_view
 
-    # def mount(_params, _session, socket) do
-    #   IO.puts("mounted")
-    #   menu_data = read_json()
-    #   IO.puts("json mount output: #{inspect(menu_data)}")
-    #   {:ok, assign(socket, data: menu_data)}
-    # end
-
-    # def read_json do
-    #   file = File.read!("priv/static/data.json")
-    #   |> Jason.decode!()
-    #   IO.puts("json output: #{inspect(file)}")
-    #   file
-    # end
-
     @spec mount(any(), any(), map()) :: {:ok, map()}
+
     def mount(_params, _session, socket) do
       menu_items =
         "priv/static/data.json"
         |> File.read!()
         |> Jason.decode!()
         # IO.puts("DearJSON: #{inspect(menu_items)}")
-      {:ok, assign(socket, :menu_items, menu_items)}
+      {:ok, assign(socket, menu_items: menu_items, is_clicked: false)}
+    end
+
+    def handle_event("handle_is_clicked", _unsigned_params, socket) do
+      update_is_clicked = !socket.assigns.is_clicked
+      {:noreply, assign(socket, is_clicked: update_is_clicked)}
     end
 
 
@@ -34,8 +26,13 @@ defmodule OrderMenuWeb.OrderMenuLive do
         <h1 class="font-bold mb-10">Desserts</h1>
         <ul class="grid grid-cols-3 gap-x-4">
         <%= for item <- @menu_items do %>
-        <li>
+        <li class="flex flex-col">
         <img class="w-[200px] h-[200px] rounded-md" src={"#{item["image"]["desktop"]}"} alt={item["name"]} />
+        <%= if @is_clicked do%>
+        <button class="bg-white border border-2 rounded-full max-w-[150px] py-1" phx-click="handle_is_clicked">ADD</button>
+        <%else%>
+        <button class="bg-white border border-2 rounded-full max-w-[150px] py-1"  phx-click="handle_is_clicked">Add to Cart</button>
+        <% end %>
         <span class="text-[#9C9591]"><%= item["category"]%></span>
         <p class="font-bold"><%= item["name"]%></p>
         <span class="text-[#c73a0f] font-bold">$<%= item["price"] %></span>
